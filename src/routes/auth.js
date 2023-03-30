f51fcdf8b25b80d71205e90b54da45b54bf911bb
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-03-30 10:30:35 -0300

    Github Fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,10 +6,29 @@ const authRouter = Router();
 const { googleAuth } = require("../handlers/authHandler");
 
 authRouter.post("/", googleAuth);
-authRouter.get("/github", passport.authenticate("auth-github", {scope: ["user:email"], session:false }));
-authRouter.get("/github/callback", passport.authenticate("auth-github", {scope: ["user:email"], session:false }),
-(req,res) => {
-    res.status(200).json(req.user)
-});
+authRouter.get(
+  "/github",
+  passport.authenticate("auth-github", {
+    scope: ["user:email"],
+    session: false,
+  })
+);
+authRouter.get(
+  "/github/callback",
+  passport.authenticate("auth-github", {
+    scope: ["user:email"],
+    session: false,
+  }),
+  (req, res) => {
+    const user = JSON.stringify(req.user);
+    res.status(200).send(`<!DOCTYPE html>
+    <html lang="en">
+    <body>
+    </body>
+    <script> window.opener.postMessage(${user}, "http://localhost:3000")
+    </script>
+    </html>`);
+  }
+);
 
 module.exports = authRouter;
