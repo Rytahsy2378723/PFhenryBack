873e2f8266a9bc72add6e76c974141c2aa87c7d2
lejesterdelacorte <86328255+lejesterdelacorte@users.noreply.github.com>
2023-04-02 13:51:45 -0300

    nada

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,17 +3,38 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../db");
 const secretKey = process.env.JWT_SECRET;
 const clientId = process.env.GOOGLE_CLIENT_ID;
-const client = new OAuth2Client(clientId);
+const clientUri = process.env.CLIENT_GOOGLE_URI;
+//const client = new OAuth2Client(clientId);
+const {google} = require('googleapis');
 
-async function verifyGoogleToken(token) {
-  console.log(token);
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: clientId,
+const client = google.auth.OAuth2(
+  secretKey,
+  clientId,
+  clientUri
+);
+
+google.options({auth: client});
+const verifyGoogleToken =async (token) => {
+  const authorizeUrl = await client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes.join(' '),
   });
-  const payload = ticket.getPayload();
-  const googleId = payload["sub"];
-  return { payload, googleId };
+  
+  const qs = token;
+  const {tokens} = await client.setToken(qs);
+  client.credentials = tokens; // eslint-disable-line require-atomic-updates
+  resolve(client);
+  return "Autenticacion lograda";
+  // console.log(token);
+  // const ticket = await client.verifyIdToken({
+  //   idToken: token,
+  //   audience: clientId,
+  // });
+  // const payload = ticket.getPayload();
+  // const googleId = payload["sub"];
+  // return { payload, googleId };
+
+
 }
 
 async function findOrCreateUser(googleUser) {
