9b76662fb0389d9cb30ae22f5a1b5a38313351e8
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-04-04 18:38:22 -0300

    Anadi cors

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+server.use(cors)
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
