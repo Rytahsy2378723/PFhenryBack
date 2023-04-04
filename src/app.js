5f22f06e9bde32a746ade0348bd9f0bc60c28d3a
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-04-04 20:53:44 -0300

    AHORA SI 100% REAL NO FAKE

--- src/app.js
+++ src/app.js
@@ -16,17 +16,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use(cors)
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors());
 
 server.use("/", routes);
 // Error catching endware.
