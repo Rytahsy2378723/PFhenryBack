eaba8dccdd87dd1c242e743fe6778dd22391ecf8
OscarMaya <mayao835@gmail.com>
2023-04-05 08:10:14 -0600

    imagen de correos arreglada

--- src/controllers/auxFunctions/orderConfirmationEmail.js
+++ src/controllers/auxFunctions/orderConfirmationEmail.js
@@ -15,7 +15,7 @@ const sendEmailOrderConfirmation = async (orderInfo, clientInfo) => {
         },
     }
     const transport = nodemailer.createTransport(config);
-    const textMessage = "<img src=`https://scontent.fntr6-1.fna.fbcdn.net/v/t39.30808-6/338441121_151457604519082_5765951118596468384_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGmvrDmONdKBRQDs_rDCAmBilL0gpm02xmKUvSCmbTbGSxD6XIyXtcvSVzgLlbbXZtrrly-4Pd2rud7AB0cy_OF&_nc_ohc=jP_HnZCrBSAAX8Zcjke&_nc_zt=23&_nc_ht=scontent.fntr6-1.fna&oh=00_AfDuYloHPjcsPxRmbA55CGDe189hbuR1juuwlvQyunhbog&oe=642D0F6E`><p>Hola " + clientInfo.name + ", tu pedido fue creado correctamente y estara siendo " +
+    const textMessage = "<img src=`https://scontent.fntr6-4.fna.fbcdn.net/v/t39.30808-6/338441121_151457604519082_5765951118596468384_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGmvrDmONdKBRQDs_rDCAmBilL0gpm02xmKUvSCmbTbGSxD6XIyXtcvSVzgLlbbXZtrrly-4Pd2rud7AB0cy_OF&_nc_ohc=M6vS0SEjQ0EAX_XD7id&_nc_zt=23&_nc_ht=scontent.fntr6-4.fna&oh=00_AfAbud6ZQ2JGnJPK9PxuRoybRsQKWPNXYQ3npPLyrWL9zw&oe=6432FE2E`><p>Hola " + clientInfo.name + ", tu pedido fue creado correctamente y estara siendo " +
         "entregado el " + orderInfo.date_delivery + " a las " + orderInfo.time_delivery + " y tendra un costo de $" +
         orderInfo.total_price + ", muchas gracias por tu preferencia.</p>"
 
